62947389c50561baaf20e6d14927b35cecb95519
Mike Johnson <mike@mapleprintservices.com>
2022-12-11 20:21:19 -0500

    expanding employee select

--- queries.js
+++ queries.js
@@ -14,7 +14,19 @@ const db = mysql.createConnection(
 
 
 const viewDept = 'SELECT * FROM department';
-const viewEmployees = 'SELECT * FROM employee';
+// const viewEmployees = 'SELECT * FROM employee';
+const viewEmployees = `SELECT 
+employee.id AS emp_id, 
+employee.first_name AS First, 
+employee.last_name AS Last, 
+employee.manager_id AS Mgr_Id, 
+role.title, 
+department.name 
+FROM employee 
+JOIN role
+ON employee.role_id = role.id  
+JOIN department
+ON employee.department_id = department.id;`
 const viewRoles = 'SELECT * FROM role';
 const addDept = `INSERT Into department`;
 
